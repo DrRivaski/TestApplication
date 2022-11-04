b45859d94105ee4e8c37e5909ec033f4a47bbce7
DrRivaski <48974215+DrRivaski@users.noreply.github.com>
2022-11-04 19:54:50 +0900

    login and sigup without firebase

--- app/src/main/java/com/example/testapplication/LoginActivity.kt
+++ app/src/main/java/com/example/testapplication/LoginActivity.kt
@@ -4,24 +4,39 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.example.testapplication.databinding.ActivityLoginBinding
+import com.example.testapplication.databinding.ActivityRegisterBinding
 
 class LoginActivity : AppCompatActivity(){
     private lateinit var btn_login:Button
     private lateinit var btn_signup:Button
     private lateinit var btn_findpwd:Button
 
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        binding= ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         btn_login=findViewById(R.id.login_btn)
         btn_signup=findViewById(R.id.sign_btn)
         btn_findpwd=findViewById(R.id.find_pwd_btn)
 
-        btn_signup.setOnClickListener{
+        binding.signBtn.setOnClickListener {
+            val intent=Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.loginBtn.setOnClickListener {
+
+        }
+
+        /*btn_signup.setOnClickListener{
             val intent=Intent(this, RegisterActivity::class.java)
             startActivity(intent)
             finish()
-        }
+        }*/
     }
 }
--- app/src/main/java/com/example/testapplication/RegisterActivity.kt
+++ app/src/main/java/com/example/testapplication/RegisterActivity.kt
@@ -4,7 +4,11 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
+import android.widget.Toast
 import com.example.testapplication.databinding.ActivityRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class RegisterActivity : AppCompatActivity(){
     private lateinit var R_btn: Button
@@ -13,14 +17,48 @@ class RegisterActivity : AppCompatActivity(){
 
     private lateinit var binding: ActivityRegisterBinding
 
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
         binding= ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        auth = Firebase.auth;
+
+
         S_btn = findViewById(R.id.submit_btn)
         R_btn = findViewById(R.id.register_btn)
-        A_btn=findViewById(R.id.Auth_button)
+        A_btn = findViewById(R.id.Auth_button)
+
+        binding.registerBtn.setOnClickListener {
+            createAccount(binding.email.text.toString(), binding.pwd.text.toString())
+            //finish()
+        }
+
+        /*R_btn.setOnClickListener {
+            createAccount()
+        }*/
+    }
+
+    private fun createAccount(email: String, password: String) {
+        if(email.isNotEmpty() && password.isNotEmpty()) {
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(
+                            this, "계정 생성 완료.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        finish() // 가입창 종료
+                    } else {
+                        Toast.makeText(
+                            this, "계정 생성 실패",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+        }
     }
 }
